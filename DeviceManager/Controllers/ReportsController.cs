bcc46e79981ee0158dd7c16ea3f4ad4dd959e52b
hidytech-a11y <hidytech@mail.com>
2025-12-18 13:03:38 +0100

    AddReport Update

---
+++ DeviceManager/Controllers/ReportsController.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using DeviceManager.Data;
+using DeviceManager.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DeviceManager.Controllers
+{
+    [Authorize(Roles = "Admin,Manager,Viewer")]
+    public class ReportsController : Controller
+    {
+        private readonly DeviceContext _context;
+
+        public ReportsController(DeviceContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var devices = _context.Devices.AsQueryable();
+
+            var model = new ReportsViewModel
+            {
+                TotalDevices = await devices.CountAsync(),
+                ActiveDevices = await devices.CountAsync(d => d.Status == "Active"),
+                InactiveDevices = await devices.CountAsync(d => d.Status == "Inactive"),
+
+                AssignedDevices = await devices.CountAsync(d => d.TechnicianId != null),
+                UnassignedDevices = await devices.CountAsync(d => d.TechnicianId == null),
+
+                AssignedTasks = await devices.CountAsync(d => d.WorkStatus == "Assigned"),
+                InProgressTasks = await devices.CountAsync(d => d.WorkStatus == "InProgress"),
+                CompletedTasks = await devices.CountAsync(d => d.WorkStatus == "Done"),
+
+                PendingApprovals = await devices.CountAsync(d => d.WorkStatus == "Done" && !d.IsApprovedByManager),
+                ApprovedTasks = await devices.CountAsync(d => d.IsApprovedByManager)
+            };
+
+            model.TechnicianStats = await _context.Technicians
+                .Select(t => new TechnicianReportItem
+                {
+                    TechnicianName = t.FullName,
+                    TotalAssigned = t.Devices.Count(),
+                    InProgress = t.Devices.Count(d => d.WorkStatus == "InProgress"),
+                    Completed = t.Devices.Count(d => d.WorkStatus == "Done")
+                })
+                .ToListAsync();
+
+            return View(model);
+        }
+    }
+
+}
+
---
+++ DeviceManager/Models/ReportsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace DeviceManager.Models
+{
+    public class ReportsViewModel
+    {
+        public int TotalDevices { get; set; }
+        public int ActiveDevices { get; set; }
+        public int InactiveDevices { get; set; }
+
+        public int AssignedDevices { get; set; }
+        public int UnassignedDevices { get; set; }
+
+        public int AssignedTasks { get; set; }
+        public int InProgressTasks { get; set; }
+        public int CompletedTasks { get; set; }
+
+        public int PendingApprovals { get; set; }
+        public int ApprovedTasks { get; set; }
+
+        public List<TechnicianReportItem> TechnicianStats { get; set; } = new();
+    }
+
+    public class TechnicianReportItem
+    {
+        public string TechnicianName { get; set; }
+        public int TotalAssigned { get; set; }
+        public int InProgress { get; set; }
+        public int Completed { get; set; }
+    }
+
+}
+
